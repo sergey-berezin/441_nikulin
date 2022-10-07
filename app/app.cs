1885d54293a4de74606338d74ac28541528ab390
MrNikulin <leonidnikulin2001@gmail.com>
2022-10-07 20:12:44 +0300

    feat 441_nikulin: add first lab

---
+++ app/app.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Diagnostics;
+using ArcFaceLibrary;
+
+namespace app
+{
+  class Program
+  {
+    static ArcFace model = new ArcFace();
+    static CancellationTokenSource cts = new CancellationTokenSource();
+    static async Task Main(string[] args)
+    {
+        try {
+            if (args.Length == 0) {
+              await test1(false);
+              // test2();
+            }
+            else {
+              var len = args.Length;
+              if (len % 2 == 1) {
+                Console.WriteLine ("Number of arguments in command line must be divisible by 2");
+                return;
+              }
+              var TArray = new Task[len/2];
+              for (int i = 0; i < len-1; i+=2) {
+                string st1 = "images/" + args[i];
+                string st2 = "images/" + args[i+1];
+                TArray[i/2] = RunTask(st1, st2, cts);
+              }
+              await Task.WhenAll(TArray);
+            }
+        }
+        catch (Exception ex) {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    static async Task test1 (bool withCancel = false) {
+      const int len = 8;
+      const int repeat = 1;
+      var input = new string[len] {"DiCaprio1.png", "DiCaprio2.png", "DuaLipa1.png", "DuaLipa2.png",
+                                "Obama1.png", "Obama2.png", "face1.png", "face2.png"};
+      for (int i = 0; i < len; i++) {
+        input[i] = "images/" + input[i];
+      }
+
+      model = new ArcFace();
+      var st = new Stopwatch();
+      st.Start();
+      var TArray = new Task[repeat * (len/2)];
+      for (int j = 0; j < repeat; j++) {
+        for (int i = 0; i < len - 1; i+=2) {
+
+          TArray[j * (len/2) + i/2] = RunTask(input[i], input[i+1], cts);
+        }
+      }
+      if (withCancel) {
+        await Task.Delay(70);
+        cts.Cancel();
+      }
+      await Task.WhenAll(TArray);
+
+      st.Stop();
+      Console.WriteLine($"Elapsed Ms async: {st.ElapsedMilliseconds}");
+    }
+    static void test2 () {
+      const int len = 8;
+      const int repeat = 1;
+      var input = new string[len] {"DiCaprio1.png", "DiCaprio2.png", "DuaLipa1.png", "DuaLipa2.png",
+                                "Obama1.png", "Obama2.png", "face1.png", "face2.png"};
+      for (int i = 0; i < len; i++) {
+        input[i] = "images/" + input[i];
+      }
+      model = new ArcFace();
+      var st = new Stopwatch();
+      st.Start();
+      for (int j = 0; j < repeat; j++) {
+        for (int i = 0; i < len - 1; i+=2) {
+          var img1 = File.ReadAllBytes(input[i]);
+          var img2 = File.ReadAllBytes(input[i+1]);
+          var List = model.Process(img1, img2);
+          lock(model) {
+            foreach (var elem in List)
+                Console.WriteLine($"\t{elem.Item1}: {elem.Item2}");
+          }
+        }
+      }
+      st.Stop();
+      Console.WriteLine($"Elapsed Ms sync: {st.ElapsedMilliseconds}");
+    }
+    static async Task RunTask (string img1, string img2, CancellationTokenSource cts)  {
+      var b1 = await File.ReadAllBytesAsync(img1);
+      var b2 = await File.ReadAllBytesAsync(img2);
+
+      var List = await model.ProcessAsync(b1, b2, cts);
+      lock(model) {
+        foreach (var elem in List)
+            Console.WriteLine($"\t{elem.Item1}: {elem.Item2}");
+      }
+    }
+  }
+}
---
+++ lib/ArcFaceLibrary.cs
@@ -0,0 +1,180 @@
+﻿// Download ONNX model from https://github.com/onnx/models/blob/main/vision/body_analysis/arcface/model/arcfaceresnet100-8.onnx
+// to project directory before run
+
+using System;
+using SixLabors.ImageSharp; // Из одноимённого пакета NuGet
+using SixLabors.ImageSharp.PixelFormats;
+using System.Linq;
+using SixLabors.ImageSharp.Processing;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using Microsoft.ML.OnnxRuntime;
+using System.Collections.Generic;
+
+namespace ArcFaceLibrary {
+    public class ArcFace
+    {
+        private InferenceSession session;
+        private SemaphoreSlim smph = new SemaphoreSlim(1, 1);
+        public ArcFace () {
+            using var modelStream = typeof(ArcFace).Assembly.GetManifestResourceStream("arcface-8.onnx");
+            using var memoryStream = new MemoryStream();
+            modelStream.CopyTo(memoryStream);
+            this.session = new InferenceSession(memoryStream.ToArray()); 
+        }
+        public async Task<List<(string, double)>> ProcessAsync (byte[] img1, byte[] img2, CancellationTokenSource cts) 
+        {
+            var img1Stream = new MemoryStream(img1);
+            var img2Stream = new MemoryStream(img2);
+
+            if (Cancelled(cts))
+                return new List<(string, double)>();
+
+            var t1 = Task<float[]>.Run (async () => {
+                var img1Stream = new MemoryStream(img1);
+                var face1 = await Image.LoadAsync<Rgb24>(img1Stream, cts.Token);
+
+                if (Cancelled(cts))
+                    return new float[0];
+
+                face1.Mutate(ctx => {
+                ctx.Resize(new ResizeOptions 
+                            {
+                                Size = new Size(112, 112),
+                                Mode = ResizeMode.Crop
+                            });
+                });
+
+                if (Cancelled(cts))
+                    return new float[0];
+
+                return GetEmbeddings(face1, cts);
+            }, cts.Token);
+
+            var t2 = Task<float[]>.Run (async () => {
+                var img2Stream = new MemoryStream(img2);
+                var face2 = await Image.LoadAsync<Rgb24>(img2Stream, cts.Token);
+
+                if (Cancelled(cts))
+                    return new float[0];
+
+                face2.Mutate(ctx => {
+                ctx.Resize(new ResizeOptions 
+                            {
+                                Size = new Size(112, 112),
+                                Mode = ResizeMode.Crop
+                            });
+                });
+
+                if (Cancelled(cts))
+                    return new float[0];
+
+                return GetEmbeddings(face2, cts);
+            }, cts.Token);
+
+            var res = await Task.WhenAll(t1, t2);
+
+            if (Cancelled(cts))
+                return new List<(string, double)>();
+
+            var L = new List<(string, double)>();
+
+            L.Add(("Distance", Distance(res[0], res[1]) * Distance(res[0], res[1])));
+
+            if (Cancelled(cts))
+                return new List<(string, double)>();
+
+            L.Add(("Similarity", Similarity(res[0], res[1])));
+            return L;
+        }
+
+        public List<(string, double)> Process (byte[] img1, byte[] img2) 
+        {
+            var img1Stream = new MemoryStream(img1);
+            var img2Stream = new MemoryStream(img2);
+
+            using var face1 = Image.Load<Rgb24>(img1Stream);
+            using var face2 = Image.Load<Rgb24>(img2Stream);
+
+            face1.Mutate(ctx => {
+                ctx.Resize(new ResizeOptions 
+                            {
+                                Size = new Size(112, 112),
+                                Mode = ResizeMode.Crop
+                            });
+            });
+
+            face2.Mutate(ctx => {
+                ctx.Resize(new ResizeOptions 
+                            {
+                                Size = new Size(112, 112),
+                                Mode = ResizeMode.Crop
+                            });
+            });
+
+            var embeddings1 = GetEmbeddings(face1);
+            var embeddings2 = GetEmbeddings(face2);
+
+            var L = new List<(string, double)>();
+            L.Add(("Distance", Distance(embeddings1, embeddings2) * Distance(embeddings1, embeddings2)));
+            L.Add(("Similarity", Similarity(embeddings1, embeddings2)));
+            return L;
+        }
+        bool Cancelled (CancellationTokenSource cts) {
+            if (cts.IsCancellationRequested)
+                return true;
+            return false;
+        }
+        float Length(float[] v) => (float)Math.Sqrt(v.Select(x => x*x).Sum());
+
+        float[] Normalize(float[] v) 
+        {
+            var len = Length(v);
+            return v.Select(x => x / len).ToArray();
+        }
+
+        float Distance(float[] v1, float[] v2) => Length(v1.Zip(v2).Select(p => p.First - p.Second).ToArray());
+
+        float Similarity(float[] v1, float[] v2) => v1.Zip(v2).Select(p => p.First * p.Second).Sum();
+
+        DenseTensor<float> ImageToTensor(Image<Rgb24> img)
+        {
+            var w = img.Width;
+            var h = img.Height;
+            var t = new DenseTensor<float>(new[] { 1, 3, h, w });
+
+            img.ProcessPixelRows(pa => 
+            {
+                for (int y = 0; y < h; y++)
+                {           
+                    Span<Rgb24> pixelSpan = pa.GetRowSpan(y);
+                    for (int x = 0; x < w; x++)
+                    {
+                        t[0, 0, y, x] = pixelSpan[x].R;
+                        t[0, 1, y, x] = pixelSpan[x].G;
+                        t[0, 2, y, x] = pixelSpan[x].B;
+                    }
+                }
+            });
+            
+            return t;
+        }
+
+        float[] GetEmbeddings(Image<Rgb24> face, CancellationTokenSource? cts = null) 
+        {
+            var inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("data", ImageToTensor(face)) };
+
+            if (cts!=null && Cancelled(cts)) 
+                return new float[0];
+
+            smph.WaitAsync();
+            IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = session.Run(inputs);
+            smph.Release();
+
+            if (cts!=null && Cancelled(cts)) 
+                return new float[0];
+
+            return Normalize(results.First(v => v.Name == "fc1").AsEnumerable<float>().ToArray());
+        }
+    }
+    
+}
